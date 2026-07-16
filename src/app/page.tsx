cce23abd1c24043975bb4c65dd0eb900eff85bcd
fukashigiNo <zemvikak@gmail.com>
2026-07-16 20:36:04 +0500

    modified styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { SideBar } from "@/widgets/sidebar";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen w-screen flex flex-col bg-[#09070f] text-white overflow-hidden">
       <SideBar />
-    </>
+    </div>
   );
 }
