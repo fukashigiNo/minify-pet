dc2f82d88d792f54823f03440f7c692fd0c5dd85
fukashigiNo <zemvikak@gmail.com>
2026-07-16 22:09:34 +0500

    modified and added bottom-player for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { PlayerBar } from "@/widgets/bottom-player";
 import { SideBar } from "@/widgets/sidebar";
+
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col bg-[#09070f] text-white overflow-hidden">
       <SideBar />
+      <PlayerBar />
     </div>
   );
 }
