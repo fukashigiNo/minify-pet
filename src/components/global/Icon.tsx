9e13f2c502a6dae90c65022caddd395143a414e9
fukashigiNo <zemvikak@gmail.com>
2026-07-16 22:08:57 +0500

    modified for add fill in component

--- src/components/global/Icon.tsx
+++ src/components/global/Icon.tsx
@@ -5,6 +5,7 @@ interface IIcon {
     icon: LucideIcon
     className?: string,
     color: string,
+    fill?: string,
     size: number,
     handlePress?: () => void
 }
@@ -13,12 +14,13 @@ export default function Icon({
     icon: IconComponent,
     className,
     color,
+    fill,
     size,
     handlePress = () => {}
 }: IIcon) {
     return (
         <div className={className} onClick={handlePress} >
-                <IconComponent  color={color} size={size} />
+                <IconComponent  color={color} size={size} fill={fill}/>
         </div>
     )
 }
