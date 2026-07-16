0b4095a27793a8fa38ca9f6be4db81ea7feaa2f6
fukashigiNo <zemvikak@gmail.com>
2026-07-16 20:37:54 +0500

    added pages for clean code

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Main()  {
+  return (
+    <div className='h-screen w-screen flex flex-col bg-[#09070f] text-white overflow-hidden'>
+      
+    </div>
+  )
+}
+
+
