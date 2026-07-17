81a233cf2afb41c55117412ed69c1a225d1c377c
fukashigiNo <zemvikak@gmail.com>
2026-07-17 22:42:34 +0500

    modified added new widget hero  for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { PlayerBar } from "@/widgets/bottom-player";
 import { SideBar } from "@/widgets/sidebar";
+import { Hero } from "@/widgets/hero";
 
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col bg-[#09070f] text-white overflow-hidden">
-      <SideBar />
+      <div className="flex flex-1 overflow-hidden overflow-x-hidden">
+        <SideBar />
+        <Hero />
+      </div>
       <PlayerBar />
     </div>
   );
