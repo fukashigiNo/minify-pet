4cc7550391e4059e09551d0cd325a840a0f60f05
fukashigiNo <zemvikak@gmail.com>
2026-07-18 22:34:41 +0500

    added redux RTK store

---
+++ src/components/store/hooks/useAppSelector.ts
@@ -0,0 +1,7 @@
+import { TypedUseSelectorHook } from "react-redux";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store";
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default useAppSelector
