35d20141009cb8ac8409cf850fa4a40a88638fff
fukashigiNo <zemvikak@gmail.com>
2026-07-19 23:33:39 +0500

    modified: global recunstruction of component to props confirm component

--- src/widgets/hero/hero.tsx
+++ src/widgets/hero/hero.tsx
@@ -2,34 +2,27 @@
 import { Icon, Button, MusicCard } from "@/components/global"
 import { Heart, Search } from "lucide-react"
 
-const mockData = [
-    {
-        photo: "kjsdkfsdfx",
-        id: 1,
-        trackName: "Midnight bloom",
-        trackAuthor: "Nova Rea",
-        trackLength: "3:24"
-    }, {
-        photo: "kjsdkfsdfx",
-        id: 2,
-        trackName: "Amber",
-        trackAuthor: "Sol Vega",
-        trackLength: "3:24"
-    }, {
-        photo: "kjsdkfsdfx",
-        id: 3,
-        trackName: "Paper Skies",
-        trackAuthor: "Lumen",
-        trackLength: "3:24"
-    }, 
-]
+interface IHero {
+    playlistId: string
+    playlistName: string
+    sumTracks: string
+    tracks: {
+        photo: string,
+        id: number,
+        trackName: string,
+        trackAuthor: string
+        trackLength: string
+    }[],
+}
 
-export default function Hero () {
+
+
+export default function Hero ({playlistId, playlistName, sumTracks, tracks}: IHero) {
   return (
-    <div className="flex flex-col items-center justify-center p-10 w-[80%] mx-auto" >
+    <div className="flex flex-col items-center justify-center p-10 w-[80%] mx-auto" id={playlistId}>
         <div className="w-[60%] flex justify-between items-center mb-3">
-            <p className="text-white text-2xl font-bold tracking-tight">Loser club-</p>
-            <p className="text-[12px]">12 tracks</p>
+            <p className="text-white text-2xl font-bold tracking-tight">{playlistName}</p>
+            <p className="text-[12px]">{sumTracks}</p>
         </div>
         <div className="flex items-center p-1 px-4 bg-[#110d1bff] w-[60%] border border-slate-600 rounded-full">
             <Icon icon={Search} size={18} color="white"/>
@@ -40,7 +33,7 @@ export default function Hero () {
             />
         </div>
         <div className="flex-1 overflow-y-auto pb-32 w-[60%] h-40 ">
-            {mockData.map((item) => (
+            {tracks.map((item) => (
                 <MusicCard 
                 key={item.id} 
                 id={item.id}  
