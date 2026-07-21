18bd3df0ea030f071d3235cdaf3f8b816733ba0a
fukashigiNo <zemvikak@gmail.com>
2026-07-21 19:32:51 +0500

    R-M: Upadet all component to props system and testing-work

--- src/widgets/bottom-player/PlayerBar.tsx
+++ src/widgets/bottom-player/PlayerBar.tsx
@@ -1,21 +1,38 @@
 "use client"
 import { Button, Icon } from "@/components/global"
 import { Play, Pause, SkipForward, SkipBack } from "lucide-react"
+import { useAppSelector, useAppDispatch } from "@/components/store/hooks"
+import { previousTrack, nextTrack } from "@/components/store/slices"
 import { useState } from "react"
 
 export default function PlayerBar ()  {
     const [play, setPlay] = useState(false)
+    const track = useAppSelector(state => state.playerSlice.currentTrack)
+    const dispatch = useAppDispatch()
+
+    if(!track) return null
+
   return (
     <div className="fixed flex items-center justify-around px-30 bottom-0 left-0 w-full h-18 bg-[#110d1bff] border-t border-zinc-500/50">
         <div className="flex items-center  gap-4">
             <div className="w-12 h-12 bg-radial-[at_25%_25%] from-[#FFFFFF]  to-[#EF33E7] to-75% rounded-[10px]" />
             <div>
-                <p className="text-[15px] font-bold">Midnight Bloom</p>
-                <p className="text-[12px] text-white/60">Nova Rea</p>
+                <p className="text-[15px] font-bold">{track.trackName}</p>
+                <p className="text-[12px] text-white/60">{track.trackAuthor}</p>
             </div>
         </div>
+        {/* Вставь этот тег внутри разметки, например, рядом с кнопками */}
+    <audio 
+        src={track?.src} 
+        autoPlay 
+        ref={(audioRef) => {
+            if (audioRef) {
+                play ? audioRef.play() : audioRef.pause();
+            }
+        }}
+    />
         <div className="flex items-center gap-6">
-            <Button className="cursor-pointer">
+            <Button className="cursor-pointer" handlePress={() => dispatch(previousTrack())}>
                 <Icon icon={SkipBack} size={16} color="#FFFFFF"/>
             </Button>
             <Button 
@@ -24,7 +41,7 @@ export default function PlayerBar ()  {
                 {play ? <Icon icon={Pause} size={19} color="black" fill="black" /> : 
                 <Icon icon={Play} size={19} color="black" fill="black" />}
             </Button>
-            <Button className="cursor-pointer">
+            <Button className="cursor-pointer " handlePress={() => dispatch(nextTrack())}>
                 <Icon icon={SkipForward} size={16} color="#FFFFFF"/>
             </Button>
         </div>
